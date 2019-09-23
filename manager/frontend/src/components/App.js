08c077fcef083d044692af8a0ab5ca3edfab1a1e
devNica <desingsdevnicaragua@gmail.com>
2019-09-23 16:13:09 -0600

    install-and-config-redux

--- manager/frontend/src/components/App.js
+++ manager/frontend/src/components/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
+import { Provider } from 'react-redux'
+import store from '../store'
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <Provider store={store}>
                 <Header />
                 <div className="container">
                     <Dashboard />
                 </div>
 
-            </div>
+            </Provider>
         );
     }
 }
--- manager/frontend/src/components/layout/Header.js
+++ manager/frontend/src/components/layout/Header.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#">React Django - LeadManager</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">React Django - LeadManager</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home
+                <span className="sr-only">(current)</span>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
                         </li>
 
                     </ul>
