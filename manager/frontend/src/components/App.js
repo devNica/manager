14b4317f601f9fd62642ae11a85b26130c909907
devNica <desingsdevnicaragua@gmail.com>
2019-09-24 09:22:39 -0600

    react-alert

--- manager/frontend/src/components/App.js
+++ manager/frontend/src/components/App.js
@@ -2,20 +2,35 @@ import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom'
 import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
+
 import { Provider } from 'react-redux'
 import store from '../store'
+
+import { Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+import Alerts from './layout/Alerts'
+
+// optional cofiguration
+const options = {
+    position: 'top center',
+    timeout: 3000,
+}
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header />
-                    <div className="container">
-                        <Dashboard />
-                    </div>
-                </Fragment>
+                <AlertProvider template={AlertTemplate} {...options}>
+                    <Fragment>
+                        <Header />
+                        <Alerts />
+                        <div className="container">
 
+                            <Dashboard />
+                        </div>
+                    </Fragment>
+                </AlertProvider>
 
             </Provider>
         );
